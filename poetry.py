7ff15e7b9fc3b1960897a12c21af466ae6c016b6
Gregor Maclaine <thegregormaclaine@gmail.com>
2020-09-23 17:26:29 +0100

    Added show_poem function

--- poetry.py
+++ poetry.py
@@ -1,1 +1,13 @@
-print('Hello World!')
+import requests
+
+url = 'https://poetrydb.org/random'
+
+def show_poem():
+	r = requests.get(url)
+	data = r.json()
+
+	print('\n' + data['title'])
+	print('By ' + data['author'] + '\n')
+	print('\n'.join(data['lines']) + '\n')
+
+show_poem()
